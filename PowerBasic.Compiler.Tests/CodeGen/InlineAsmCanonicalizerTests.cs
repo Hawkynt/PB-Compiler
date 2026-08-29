500b32ae6fb0ce189130a2796b3c455a3777d5bc
Hawkynt <github@synthelicz.de>
2026-08-29 23:41:08 +0200

    # cover target-dependent canonicalization guard

--- PowerBasic.Compiler.Tests/CodeGen/InlineAsmCanonicalizerTests.cs
+++ PowerBasic.Compiler.Tests/CodeGen/InlineAsmCanonicalizerTests.cs
@@ -5,15 +5,9 @@ namespace PowerBasic.Compiler.Tests.CodeGen;
 [TestFixture]
 public sealed class InlineAsmCanonicalizerTests {
   [TestCase("MOV AX, AX")]
-  [TestCase("mov eax, eax ; redundant")]
-  [TestCase("MOVDQA XMM3, XMM3")]
-  [TestCase("MOVDQU xmm7, xmm7")]
-  [TestCase("PAND XMM1, XMM1")]
-  [TestCase("POR XMM1, XMM1")]
-  [TestCase("PMINSB XMM2, XMM2")]
-  [TestCase("PMAXUD XMM2, XMM2")]
-  [TestCase("PBLENDW XMM4, XMM4, 170")]
-  public void IsRedundant_GivenRegisterIdentity_ThenReturnsTrue(string line) {
+  [TestCase("mov bx, bx ; redundant")]
+  [TestCase("MOV AL, AL")]
+  public void IsRedundant_GivenBaselineRegisterIdentity_ThenReturnsTrue(string line) {
     Assert.That(InlineAsmCanonicalizer.IsRedundant(line), Is.True);
   }
 
@@ -21,11 +15,14 @@ public sealed class InlineAsmCanonicalizerTests {
   [TestCase("XOR AX, AX")]
   [TestCase("MOV AX, BX")]
   [TestCase("MOV [BX], AX")]
+  [TestCase("MOV EAX, EAX")]
+  [TestCase("MOVDQA XMM3, XMM3")]
+  [TestCase("PAND XMM1, XMM1")]
+  [TestCase("PMAXUD XMM2, XMM2")]
+  [TestCase("PBLENDW XMM4, XMM4, 170")]
   [TestCase("VMOVDQA XMM0, XMM0")]
-  [TestCase("PCMPEQQ XMM0, XMM0")]
-  [TestCase("PBLENDW XMM0, XMM0, mask")]
   [TestCase("MOV DS, DS")]
-  public void IsRedundant_GivenObservableOrInvalidCase_ThenReturnsFalse(string line) {
+  public void IsRedundant_GivenObservableInvalidOrTargetDependentCase_ThenReturnsFalse(string line) {
     Assert.That(InlineAsmCanonicalizer.IsRedundant(line), Is.False);
   }
 }
