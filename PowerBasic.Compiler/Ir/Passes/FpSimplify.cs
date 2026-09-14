e752855c38d52a0724c80105a3e97d8cc7707167
Hawkynt <github@synthelicz.de>
2026-09-14 10:43:42 +0200

    * reuse shared floating-point analyses

--- PowerBasic.Compiler/Ir/Passes/FpSimplify.cs
+++ PowerBasic.Compiler/Ir/Passes/FpSimplify.cs
@@ -23,13 +23,23 @@ public static class FpSimplify {
   public static int Run(IrFunction function) => Run(function, IrFastMathFlags.None);
 
   internal static int Run(IrFunction function, IrFastMathFlags assumptions) {
+    ArgumentNullException.ThrowIfNull(function);
+    return Run(function, assumptions, new IrAnalysisManager(function)).Changes;
+  }
+
+  internal static IrPassResult Run(IrFunction function, IrFastMathFlags assumptions, IrAnalysisManager analyses) {
+    ArgumentNullException.ThrowIfNull(function);
+    ArgumentNullException.ThrowIfNull(analyses);
     if (function.HasErrorHandler || function.HasInlineAsm)
-      return 0;
-    var ranges = IrRangeAnalysis.Build(function);
-    var domains = FpDomainAnalysis.Build(function);
+      return IrPassResult.Unchanged;
+
+    var ranges = analyses.Get(IrAnalyses.Ranges);
+    var domains = analyses.Get(IrAnalyses.FpDomains);
     var changes = SimplifyClassifications(function, assumptions, ranges, domains);
     changes += NarrowDemandedPrecision(function, assumptions);
-    return changes;
+    return changes == 0
+      ? IrPassResult.Unchanged
+      : IrPassResult.ChangedPreserving(changes, IrAnalyses.Dominators, IrAnalyses.Loops);
   }
 
   private static int SimplifyClassifications(IrFunction function, IrFastMathFlags assumptions,
